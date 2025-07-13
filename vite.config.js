559810ce057bb7b7c84169d27ef0e94aa946f6ba
Alvaro González Gallardo <mscinf12@masaveu.net>
2025-07-13 22:54:48 +0200

    PRIMERA APP

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Eventos Asturias',
+        short_name: 'EventosAst',
+        description: 'Cosas que hacer en Asturias',
+        theme_color: '#673ab7',
+        background_color: '#ffffff',
+        display: 'standalone',
+        orientation: 'portrait',
+        start_url: '/',
+        icons: [
+          {
+            src: '/helena.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/helena.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      devOptions: {
+        enabled: true
+      }
+    })
+  ]
 })
